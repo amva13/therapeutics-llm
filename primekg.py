e5fb58b3aa765a395da40ecb078094cf65b4b7ba
Alejandro Velez-Arce <alex@alexv24.com>
2024-09-28 18:57:15 -0400

    mend

--- primekg.py
+++ primekg.py
@@ -6,8 +6,8 @@ pkgdf = pkg.get_data()
 
 def get_all_drug_evidence(disease):
     """given a disease, retrieve all drugs interacting with proteins relevant to disease"""
-    prots = pkgdf[pkgdf["relation"] == "disease_protein" & pkgdf["x_name"] == disease]["y_name"].unique()
-    drugs = pkgdf[(pkgdf["relation"] == "drug_protein") & pkgdf["y_name"].isin(prots)]
+    prots = pkgdf[(pkgdf["relation"] == "disease_protein") & (pkgdf["x_name"] == disease)]["y_name"].unique()
+    drugs = pkgdf[(pkgdf["relation"] == "drug_protein") & (pkgdf["y_name"].isin(prots))]
     relations = drugs["display_relation"].unique()
     out = {}
     for rel in relations:
@@ -15,15 +15,25 @@ def get_all_drug_evidence(disease):
     return out
 
 def get_all_associated_targets(disease):
-    return pkgdf[pkgdf["relation"] == "disease_protein" & pkgdf["x_name"] == disease][["y_name", "display_relation"]].unique()
+    return pkgdf[(pkgdf["relation"] == "disease_protein") & (pkgdf["x_name"] == disease)][["y_name", "display_relation"]]
 
 def get_disease_disease_associations(disease):
-    return pkgdf[(pkgdf["relation"] == "disease_disease") & (pkgdf["x_name"] == disease)]["y_name", "display_relation"].unique()
+    return pkgdf[(pkgdf["relation"] == "disease_disease") & (pkgdf["x_name"] == disease)][["y_name", "display_relation"]]
 
 def get_labels_from_evidence(disease):
     diseases = get_disease_disease_associations(disease)["y_name"]
     out = set()
     for d in diseases:
-        targets = get_all_associated_targets(d)
+        targets = get_all_associated_targets(d)["y_name"].unique()
         out.update(targets)
     return list(out)
+
+def all_diseases_by_keyword(kw):
+    return pkgdf[(pkgdf["relation"] == "disease_protein") & (pkgdf["x_name"].str.contains(kw, case=False, na=False))]["x_name"].unique()
+
+if __name__ == "__main__":
+    x = all_diseases_by_keyword("autism")
+    [get_all_drug_evidence(d) for d in x]
+    [get_all_associated_targets(d) for d in x]
+    [get_disease_disease_associations(d) for d in x]
+    print([get_labels_from_evidence(d) for d in x])
